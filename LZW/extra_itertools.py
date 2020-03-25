d05a7b627a4703a66e8c14df64531785cb24986b
MapleCCC <littlelittlemaple@gmail.com>
2020-03-26 01:32:53 +0800

    Update extra_itertools.py

--- LZW/extra_itertools.py
+++ LZW/extra_itertools.py
@@ -1,43 +1,80 @@
-from typing import *
 from itertools import groupby, zip_longest
+from typing import *
 
 
-TAIL_EMPTY_SENTINEL = object()
+class EmptyTailError(Exception):
+    pass
 
 
-def remove_tail(iterable: Iterable, n: int = 1, store_tail: bool = True) -> Iterator:
+class NotReachedTailError(Exception):
+    pass
+
+
+# class disguised as function
+class remove_tail:
     """
-    Store tail in function object's attribute named "tail", for later refernce.
-
-    You can ask it to not store the tail, so as to save space,
-    by setting parameter store_tail to False.
+    Store tail in iterator object's attribute named "tail", for later refernce.
 
     Be aware that the tail storing behavior may give you surprising result when
     you expect some references to be garbage collected.
-
-    If passed in an zero-length iterable, which means there is no tail, a singleton
-    TAIL_EMPTY_SENTINEL would be stored in remove_tail.tail instead.
     """
-    if n != 1:
-        raise NotImplementedError
 
-    lookahead = TAIL_EMPTY_SENTINEL
-    for i, elem in enumerate(iterable):
-        if i != 0:
-            yield lookahead
-        lookahead = elem
+    # If passed in an zero-length iterable, which means there exists no tail, a class
+    # level constant singleton _TAIL_EMPTY_SENTINEL would be stored in tail instead.
 
-    if store_tail:
-        remove_tail.tail = lookahead
+    _TAIL_EMPTY_SENTINEL = object()
+
+    def __init__(self, iterable: Iterable, n: int = 1) -> None:
+        if n != 1:
+            raise NotImplementedError
+
+        self._past = self.__class__._TAIL_EMPTY_SENTINEL
+        self._internal_iterator = enumerate(iterable)
+        self._reached_tail = False
+
+    def __next__(self) -> Any:
+        try:
+            i, elem = next(self._internal_iterator)
+            if i == 0:
+                self._past = elem
+                ret = self._past
+                _, second_elem = next(self._internal_iterator)
+                self._past = second_elem
+                return ret
+            else:
+                ret = self._past
+                self._past = elem
+                return ret
+        except StopIteration:
+            self._reached_tail = True
+            raise
+
+    def __iter__(self) -> Iterator:
+        return self
+
+    def reached_tail(self) -> bool:
+        return self._reached_tail
+
+    def has_empty_tail(self) -> bool:
+        return self._past is self.__class__._TAIL_EMPTY_SENTINEL
+
+    @property
+    def tail(self) -> Any:
+        if not self._reached_tail:
+            raise NotReachedTailError("Not yet reached tail")
+        if self.has_empty_tail():
+            raise EmptyTailError("Empty iterable has no tail")
+        return self._past
 
 
 def ijoin(separator: Iterable, iterables: Iterable[Iterable]) -> Iterable:
     sep = list(separator)
-    for iterable in remove_tail(iterables):
+    new_iterator = remove_tail(iterables)
+    for iterable in new_iterator:
         yield from iterable
         yield from sep
-    if remove_tail.tail != TAIL_EMPTY_SENTINEL:
-        yield from remove_tail.tail
+    if new_iterator.reached_tail() and not new_iterator.has_empty_tail():
+        yield from new_iterator.tail
 
 
 # The all_equal function comes from more-itertools library
@@ -146,3 +183,13 @@ if __name__ == "__main__":
     print(list(a))
     a = next(s)
     print(list(a))
+
+
+def takeuntil(iterable: Iterable, sentinel: Any) -> Iterator:
+    stop_signal = False
+    for elem in iterable:
+        if elem == sentinel:
+            stop_signal = True
+        yield elem
+        if stop_signal:
+            return
