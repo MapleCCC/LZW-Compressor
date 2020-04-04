a3192821d07639ddbf97600a6db795b8a8039801
MapleCCC <littlelittlemaple@gmail.com>
2020-04-04 18:50:47 +0800

    Fix a bug so that test_bitarray.test_repr passes

--- LZW/bit.py
+++ LZW/bit.py
@@ -4,3 +4,8 @@ from enum import IntEnum
 class Bit(IntEnum):
     ZERO = 0
     ONE = 1
+
+    def __repr__(self) -> str:
+        # Enum's default __repr__ can't ensure the invariant "eval(repr(x)) == x"
+        # So we override its default __repr__ with __str__
+        return str(self)
